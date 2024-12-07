89a61104a59e6f1306a8d5fe458d11300f6dcb9e
tim <tymur@buiadzhi.com>
2024-12-07 14:31:33 +0000

    Solved 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    // Validate input
+    if (times < 0) {
+        return "ERROR";
+    }
 
+    let result = ""; // Initialize an empty result string
+
+    // Repeat the word 'times' times
+    while (times > 0) {
+        result += word; // Append the word to the result
+        times--; // Decrement the counter
+    }
+
+    return result; // Return the repeated string
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
