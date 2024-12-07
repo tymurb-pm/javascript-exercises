1ec1cec46d275d485ff2e992e61b0a8f3131cf29
tim <tymur@buiadzhi.com>
2024-12-07 18:56:15 +0000

    solved 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...removeArgument) {
+   let index = 0;
+   while (index<array.length) {
+    if (removeArgument.includes(array[index])) {
+        array.splice(index, 1);
+    } else {
+        ++index;
+    }
+    }
+    console.log(array);
+    return array
+   };
 
 // Do not edit below this line
 module.exports = removeFromArray;
